0f5df83182d2bcda63e5faaf3579e9ef986d72fa
Priyanshu Belwal <priyanshubelwalpb@gmail.com>
2024-08-16 00:36:04 +0900

    first commit

---
+++ src/main/java/com/priyanshu/jsu/JavaSpringUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.priyanshu.jsu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringUtilsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/priyanshu/jsu/JavaSpringUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priyanshu.jsu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringUtilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
